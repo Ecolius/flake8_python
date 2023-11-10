e45b04ce3e79987f92cf4c3c7da0d02c72115771
Ecolius <79538539+Ecolius@users.noreply.github.com>
2023-11-09 22:56:51 -0300

    dependencias

--- fila_prioritaria.py
+++ fila_prioritaria.py
@@ -8,23 +8,28 @@ from constantes import CODIGO_PRIORITARIO
 class FilaPrioritaria(FilaBase):
 
     def gera_senha_atual(self) -> None:
-        self.senha_atual = f'${CODIGO_PRIORITARIO}{self.codigo}'
+        self.senha_atual = f'{CODIGO_PRIORITARIO}{self.codigo}'
 
     def chama_cliente(self, caixa: int) -> str:
         cliente_atual: str = self.fila.pop(0)
         self.clientes_atendidos.append(cliente_atual)
         return (f'Cliente atual: {cliente_atual}, dirija-se ao caixa: {caixa}')
 
-    def estatistica(self, dia: str, agencia: int, flag: str) -> dict:
-        estatistica: Dict[str, Union[List[str], str, int]] = {}
-        if flag != 'detail':
-            estatistica[f'{agencia}-{dia}'] = len(self.clientes_atendidos)
-        else:
-            estatistica['dia'] = dia
-            estatistica['agencia'] = agencia
-            estatistica['clientes_atendidos'] = self.clientes_atendidos
-            estatistica['quantidade_clientes_atendidos'] = (
-                len(self.clientes_atendidos)
-            )
+    def estatistica(self, dia: str, agencia: int, retorna_estatistica) -> dict:
+        estatistica = retorna_estatistica(dia, agencia)
 
-        return estatistica
+        return estatistica.roda_estatistica(self.clientes_atendidos)
+
+    # def estatistica(self, dia: str, agencia: int, flag: str) -> dict:
+    #     estatistica: Dict[str, Union[List[str], str, int]] = {}
+    #     if flag != 'detail':
+    #         estatistica[f'{agencia}-{dia}'] = len(self.clientes_atendidos)
+    #     else:
+    #         estatistica['dia'] = dia
+    #         estatistica['agencia'] = agencia
+    #         estatistica['clientes_atendidos'] = self.clientes_atendidos
+    #         estatistica['quantidade_clientes_atendidos'] = (
+    #             len(self.clientes_atendidos)
+    #         )
+    #
+    #     return estatistica
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 # from fila_normal import FilaNormal
 # from fila_prioritaria import FilaPrioritaria
 from fabrica_fila import FabricaFila
+from estatistica_detalhada import EstatisticaDetalhada
+from estatistica_resumida import EstatisticaResumida
+
 
 # fila_teste = filanormal()
 #
@@ -22,8 +25,12 @@ from fabrica_fila import FabricaFila
 # print(fila_teste_2.chama_cliente(1))
 # print(fila_teste_2.estatistica('10/01/1993', 215, 'detail'))
 
-teste_fabrica = FabricaFila().pega_fila('normal')
+teste_fabrica = FabricaFila().pega_fila('prioritaria')
+teste_fabrica.atualiza_fila()
 teste_fabrica.atualiza_fila()
 teste_fabrica.atualiza_fila()
 teste_fabrica.atualiza_fila()
 print(teste_fabrica.chama_cliente(10))
+print(teste_fabrica.chama_cliente(10))
+print(teste_fabrica.chama_cliente(10))
+print(teste_fabrica.estatistica('20/03/2025', 120, EstatisticaResumida))
