f24a69cee87ac176bd1d62a04d84428dfd542e20
Ecolius <79538539+Ecolius@users.noreply.github.com>
2023-11-09 23:03:51 -0300

    criando injecao de dependencia

--- fila_prioritaria.py
+++ fila_prioritaria.py
@@ -1,8 +1,12 @@
-from typing import Dict, List, Union
+from typing import Union
 
 
 from fila_base import FilaBase
 from constantes import CODIGO_PRIORITARIO
+from estatistica_resumida import EstatisticaResumida
+from estatistica_detalhada import EstatisticaDetalhada
+
+Classes = Union[EstatisticaResumida, EstatisticaDetalhada]
 
 
 class FilaPrioritaria(FilaBase):
@@ -15,10 +19,9 @@ class FilaPrioritaria(FilaBase):
         self.clientes_atendidos.append(cliente_atual)
         return (f'Cliente atual: {cliente_atual}, dirija-se ao caixa: {caixa}')
 
-    def estatistica(self, dia: str, agencia: int, retorna_estatistica) -> dict:
-        estatistica = retorna_estatistica(dia, agencia)
+    def estatistica(self, retorna_estatistica: Classes) -> dict:
 
-        return estatistica.roda_estatistica(self.clientes_atendidos)
+        return retorna_estatistica.roda_estatistica(self.clientes_atendidos)
 
     # def estatistica(self, dia: str, agencia: int, flag: str) -> dict:
     #     estatistica: Dict[str, Union[List[str], str, int]] = {}
